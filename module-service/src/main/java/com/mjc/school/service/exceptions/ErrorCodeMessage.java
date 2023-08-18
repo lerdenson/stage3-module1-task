fe1a288ad4d67a0e3b42d5129f0d74f8cf75e397
Максим <Maxim.d.morozov@gmail.com>
2023-08-18 23:30:19 +0400

    renaming

--- module-service/src/main/java/com/mjc/school/service/exceptions/ErrorCodeMessage.java
+++ module-service/src/main/java/com/mjc/school/service/exceptions/ErrorCodeMessage.java
@@ -1,11 +1,11 @@
 package com.mjc.school.service.exceptions;
 
 public enum ErrorCodeMessage {
-    NEWS_NOT_FOUND("01", "NewsModel with id: %d not found"),
-    AUTHOR_NOT_FOUND("02", "AuthorModel with id: %d not found"),
+    NEWS_NOT_FOUND("01", "News with id: %d not found"),
+    AUTHOR_NOT_FOUND("02", "Author with id: %d not found"),
     INCORRECT_NEWS_TITLE("03", "Title should`t be shorter than 5 characters and longer than 30. Your title length is: %d"),
     INCORRECT_NEWS_CONTENT("04", "Content should`t be shorter than 5 characters and longer than 255. Your content length is: %d"),
-    NULL_AUTHOR_ID("05", "AuthorModel id can`t be null");
+    NULL_AUTHOR_ID("05", "Author id can`t be null");
 
     private final String code;
     private final String message;
